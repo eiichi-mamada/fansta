8d3809c55cfdf4cc5802f821b594e4be2963ab9b
eiichi-mamada <eastrage@gmail.com>
2020-09-08 00:12:14 +0900

    ユーザー投稿一覧の追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   
   root to: 'toppages#index'
   resources :users, only: [:show]
+  get '/user_posts/:id', to: 'users#user_posts', as: 'user_posts'
   resources :teams, only: [:index,:create,:destroy,:show, :update]
   resources :posts, except: [:index]
 
