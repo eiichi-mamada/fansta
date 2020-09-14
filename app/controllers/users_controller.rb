3474c6cbe689650ab27ab2423116d29aeb509e1d
eiichi-mamada <eastrage@gmail.com>
2020-09-14 17:17:23 +0900

    リファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,27 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user
+
   def show
-    @user = User.find(params[:id])
     @favorites = @user.favorites_posts.order(id: :desc).page(params[:page]).per(2)
     counts(@user)
   end
   
   def posts
-    @user = User.find(params[:id])
   end
   
   def followings
-    @user = User.find(params[:id])
     @followings = @user.followings.order(id: :desc).page(params[:page]).per(5)
     counts(@user)
   end
 
   def fan_teams
-    @user = User.find(params[:id])
     @fan_teams = @user.teams.page(params[:page]).per(5)
     counts(@user)
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
