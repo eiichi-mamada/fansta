dc2f60ee7d0e942b62eafa4d0f93bdb03c05e930
eiichi-mamada <eastrage@gmail.com>
2021-12-31 00:11:10 +0900

    syslog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.logger = Logger::Syslog.new
+  config.logger = Logger::Syslog.new('appname', Syslog::LOG_LOCAL7)
+  
 end
