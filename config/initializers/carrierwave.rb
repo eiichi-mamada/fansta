f62f3ed5a6a8b024ca5d6e22593455f62cc878e6
eiichi-mamada <eastrage@gmail.com>
2022-01-01 09:07:22 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,15 +4,15 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production? # 本番環境:AWS使用
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
       aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
       region: 'ap-northeast-1'
     }
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'fansta-s3-bucket'
+    config.fog_directory = 'fansta-s3-bucket'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fansta-s3-bucket'
   else
     config.storage :file # 開発環境:public/uploades下に保存
