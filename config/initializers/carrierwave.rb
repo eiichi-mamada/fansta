fc59dfa168f3629490ef2a765d74a555c732180b
eiichi-mamada <eastrage@gmail.com>
2022-01-03 10:55:28 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production? # 本番環境:AWS使用
+  if Rails.env.production? # 本番環境:AWS S3を使用
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -12,6 +12,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
       region: 'ap-northeast-1'
     }
+    config.fog_public = false
     config.fog_directory = 'fansta-s3-bucket'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fansta-s3-bucket'
   else
